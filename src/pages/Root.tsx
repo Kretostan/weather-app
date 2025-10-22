de67ae8a36aa215186784d9fc187adfc1a204b9e
Kretostan <kubakret82@icloud.com>
2025-10-22 11:55:05 +0200

    fix: add correct path for development an production

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router";
+import backgroundImage from "../assets/background.webp";
 
 const RootLayout = () => {
     window.addEventListener("load", () => {
-        document.body.style.backgroundImage = "url('/src/assets/background.webp')";
+        document.body.style.backgroundImage = `url(${backgroundImage})`;
     });
 
     return <div className="flex flex-1 flex-col items-center gap-12 sm:gap-16 py-12 sm:py-16 min-h-screen">
