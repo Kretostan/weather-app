648e95f666003b6493685541bff2d1831f5272a6
Kretostan <kubakret82@icloud.com>
2025-10-22 11:50:52 +0200

    perf: set background asynchronously

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,10 @@
 import { Outlet } from "react-router";
 
 const RootLayout = () => {
+    window.addEventListener("load", () => {
+        document.body.style.backgroundImage = "url('/src/assets/background.webp')";
+    });
+
     return <div className="flex flex-1 flex-col items-center gap-12 sm:gap-16 py-12 sm:py-16 min-h-screen">
         <Outlet />
     </div>
