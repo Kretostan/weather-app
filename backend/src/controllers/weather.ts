fdb2a2f6a9c646d9208569b7113c163073aa7cf4
Kretostan <kubakret82@icloud.com>
2026-01-02 23:37:29 +0100

    refactor: change response object

--- backend/src/controllers/weather.ts
+++ backend/src/controllers/weather.ts
@@ -1,19 +1,8 @@
 import axios from "axios";
 import type { Request, Response } from "express";
-import type {
-	WeatherDayData,
-	WeatherHourData,
-	WeatherRequest,
-	WeatherResponse,
-	WeatherSuccessResponse,
-} from "../types";
+import type { WeatherRequest, WeatherResponse } from "../types";
 
 const API_KEY = String(process.env.WEATHER_API_KEY);
-
-const normalizeIconUrl = (icon: string) =>
-	icon.startsWith("//") ? `https:${icon}` : icon;
-
-// TODO: Zrobić oddzielnie current weather i forecast w celu optymalizacji zapytań
 
 export const fetchWeather = async (
 	req: Request<unknown, unknown, WeatherRequest>,
@@ -29,56 +18,15 @@ export const fetchWeather = async (
 		}
 		const response = await axios.get(forecastUrl);
 		const data = response.data;
-		// TODO: Ogarnąć, możliwe, że ze strony zwraca wystarczające informacje i nie muszę takiego dużego obiektu zwracać ręcznie tylko po prostu response.data
-		const forecastData: WeatherSuccessResponse = {
-			city: data.location.name,
-			country: data.location.country,
-			current: {
-				temp_c: Math.round(data.current.temp_c),
-				temp_f: Math.round(data.current.temp_f),
-				condition: {
-					weather: data.current.condition.text,
-					icon: normalizeIconUrl(data.current.condition.icon),
-				},
-				wind_mph: data.current.wind_mph,
-				wind_kph: data.current.wind_kph,
-				pressure_mb: data.current.pressure_mb,
-				humidity: data.current.humidity,
-				feelslike_c: Math.round(data.current.feelslike_c),
-				feelslike_f: Math.round(data.current.feelslike_f),
-			},
-			forecast: data.forecast.forecastday.map((day: WeatherDayData) => {
-				return {
-					date: day.date,
-					day: {
-						avgtemp_c: Math.round(day.day.avgtemp_c),
-						avgtemp_f: Math.round(day.day.avgtemp_f),
-						condition: {
-							text: day.day.condition.text,
-							icon: day.day.condition.icon,
-						},
-					},
-					hour: day.hour.map((hourData: WeatherHourData) => {
-						// TODO: Porównać hourData.time z aktualnym czasem, aby dodać tylko aktualną oraz przyszłe godziny dla dzisiejszej daty
-						return {
-							time: hourData.time,
-							temp_c: Math.round(hourData.temp_c),
-							temp_f: Math.round(hourData.temp_f),
-							condition: {
-								text: hourData.condition.text,
-								icon: hourData.condition.icon,
-							},
-							feelslike_c: Math.round(hourData.feelslike_c),
-							feelslike_f: Math.round(hourData.feelslike_f),
-							chance_of_rain: hourData.chance_of_rain,
-						};
-					}),
-				};
-			}),
+		const weatherData = {
+			location: data.location,
+			current: data.current,
+			forecast: data.forecast.forecastday,
 		};
-		return res.json(forecastData);
-	} catch (_error) {
+		return res.json(weatherData);
+	} catch (error) {
 		// TODO: Error handling
+		console.error(error);
 		return res.status(500).json({
 			error: "An error occurred while fetching weather data",
 		});
--- backend/src/types/weather.ts
+++ backend/src/types/weather.ts
@@ -4,51 +4,9 @@ export interface WeatherRequest {
 }
 
 export interface WeatherSuccessResponse {
-	city: string;
-	country: string;
-	current: WeatherCurrentData;
-	forecast: WeatherDayData[];
-}
-
-interface WeatherCurrentData {
-	temp_c: number;
-	temp_f: number;
-	condition: {
-		weather: string;
-		icon: string;
-	};
-	wind_mph: number;
-	wind_kph: number;
-	pressure_mb: number;
-	humidity: number;
-	feelslike_c: number;
-	feelslike_f: number;
-}
-
-export interface WeatherDayData {
-	date: string;
-	day: {
-		avgtemp_c: number;
-		avgtemp_f: number;
-		condition: {
-			text: string;
-			icon: string;
-		};
-	};
-	hour: WeatherHourData[];
-}
-
-export interface WeatherHourData {
-	time: string;
-	temp_c: number;
-	temp_f: number;
-	condition: {
-		text: string;
-		icon: string;
-	};
-	feelslike_c: number;
-	feelslike_f: number;
-	chance_of_rain: number;
+	location: object;
+	current: object;
+	forecast: object;
 }
 
 interface WeatherErrorResponse {
