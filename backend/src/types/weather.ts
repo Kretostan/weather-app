7702db084a2615f2fde04cb5b14bc3d926c574ec
Kretostan <kubakret82@icloud.com>
2026-01-02 23:37:38 +0100

    refactor: change response object

--- backend/src/types/weather.ts
+++ backend/src/types/weather.ts
@@ -5,7 +5,20 @@ export interface WeatherRequest {
 
 export interface WeatherSuccessResponse {
 	location: object;
-	current: object;
+	current: {
+		temp_c: number;
+		temp_f: number;
+		condition: {
+			weather: string;
+			icon: string;
+		};
+		wind_mph: number;
+		wind_kph: number;
+		pressure_mb: number;
+		humidity: number;
+		feelslike_c: number;
+		feelslike_f: number;
+	};
 	forecast: object;
 }
 
