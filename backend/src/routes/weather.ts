566d8c7968e6b608b1499ecd1d80d4f437c4b94f
Kretostan <kubakret82@icloud.com>
2026-01-08 14:30:41 +0100

    refactor: remove unnecessary health check

--- backend/src/routes/weather.ts
+++ backend/src/routes/weather.ts
@@ -1,13 +1,8 @@
-import type { Request, Response } from "express";
 import { Router } from "express";
 
 import { fetchWeather } from "../controllers/weather";
 
 const router = Router();
-
-router.get("/", (_req: Request, res: Response) => {
-	return res.send("Backend is running!");
-});
 
 router.post("/weather", fetchWeather);
 
