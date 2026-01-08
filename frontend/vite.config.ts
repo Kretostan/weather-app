765c9843eab1ce7455b0c0a24b1880c90ca5dcf0
Kretostan <kubakret82@icloud.com>
2026-01-08 14:22:17 +0100

    fix: change proxy for Vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
 				"/api": {
 					target: backendTarget,
 					changeOrigin: true,
-					rewrite: (path) => path.replace(/^\/api/, ""),
+					rewrite: (path) => path.replace(/^\/api\/weather/, ""),
 				},
 			},
 		},
